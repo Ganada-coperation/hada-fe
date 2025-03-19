e751080c002ed6b460ec3a78bde22478b73da51e
heyn2 <hy702@gachon.ac.kr>
2025-03-19 17:17:36 +0900

    🆕 feat(button): 공통 버튼 컴포넌트 추가

---
+++ src/app/components/common/Button.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { gowunBatang } from "@/app/styles/fonts";
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+export default function Button({ text, onClick, disabled = false }: ButtonProps) {
+  return (
+    <StyledButton onClick={onClick} disabled={disabled}>
+      {text}
+    </StyledButton>
+  );
+}
+
+// ✅ 공통 버튼 스타일
+const StyledButton = styled.button`
+   font-family: ${gowunBatang.style.fontFamily};;
+  margin-top: 20px;
+  width: 200px;
+  padding: 12px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: white;
+  border: none;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.textMuted};
+    cursor: not-allowed;
+  }
+
+  &:hover:enabled {
+    background-color: ${({ theme }) => theme.colors.secondary};
+  }
+`;
