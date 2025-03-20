2649bf71371f5bc7c5bc93cc392fcdc04cf77be5
heyn2 <hy702@gachon.ac.kr>
2025-03-20 14:22:05 +0900

    🎨 style(button): 버튼 스타일 조정 및 홈 페이지에서 맞춤 스타일 적용

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -5,11 +5,12 @@ interface ButtonProps {
   text: string;
   onClick: () => void;
   disabled?: boolean;
+  className?: string; // ✅ className 추가
 }
 
-export default function Button({ text, onClick, disabled = false }: ButtonProps) {
+export default function Button({ text, onClick, disabled = false, className }: ButtonProps) {
   return (
-    <StyledButton onClick={onClick} disabled={disabled}>
+    <StyledButton onClick={onClick} disabled={disabled} className={className}>
       {text}
     </StyledButton>
   );
@@ -17,7 +18,7 @@ export default function Button({ text, onClick, disabled = false }: ButtonProps)
 
 // ✅ 공통 버튼 스타일
 const StyledButton = styled.button`
-   font-family: ${gowunBatang.style.fontFamily};;
+  font-family: ${gowunBatang.style.fontFamily};
   margin-top: 20px;
   width: 200px;
   padding: 12px;
