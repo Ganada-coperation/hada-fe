681af88d4c2bd0e37e81f020955dc079f3a38b92
shimfff <shimseawon1510@gmail.com>
2025-03-25 23:40:07 +0900

    :sparkles: feat : 넥스트 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'standalone', // Docker로 배포 시 꼭 필요!
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        styledComponents: true, // ✅ 꼭 필요
+    },
 };
 
 export default nextConfig;
