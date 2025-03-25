62763b74495dfac9e4b926ed07e98181ca0978a2
heyn2 <hy702@gachon.ac.kr>
2025-03-25 20:58:47 +0900

    fade in 삭제

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -136,10 +136,6 @@ export default function WritePage() {
   );
 }
 
-const fadeIn = keyframes`
-  from { opacity: 0; transform: translateY(-10px); }
-  to { opacity: 1; transform: translateY(0); }
-`;
 
 const StyledWriteWrapper = styled.div`
   display: flex;
