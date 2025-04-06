2f828974c2381e479bbacc5b8aae23f7e2e0cb98
heyn2 <hy702@gachon.ac.kr>
2025-04-06 22:15:37 +0900

    🛠️ build: 프로젝트 설정 및 의존성 업데이트

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/app/components"),
+      "@modals": path.resolve(__dirname, "src/app/(modals)"),
+      "@services": path.resolve(__dirname, "src/app/services"),
+      "@utils": path.resolve(__dirname, "src/app/utils"),
+      "@styles": path.resolve(__dirname, "src/app/styles"),
+      "@config": path.resolve(__dirname, "src/app/config"),
+
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
