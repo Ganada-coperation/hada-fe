1584003d4b336511885d57266c74183712e3a922
heyn2 <hy702@gachon.ac.kr>
2025-04-07 19:44:34 +0900

    🐛 fix: checkNickname 함수 기본값 추가로 오류 방지

--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -1,6 +1,7 @@
 import { apiRequest } from "./api";
 
-// 닉네임 중복 확인 API 호출
 export async function checkNickname(nickname: string) {
-  return apiRequest(`/nickname?nickname=${nickname}`);
+  return apiRequest(`/nickname?nickname=${nickname}`, {
+    method: "GET",
+  });
 }
