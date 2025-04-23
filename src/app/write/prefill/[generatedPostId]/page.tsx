b34a00f0c3e41c2e073ea65b8c82ba49a927126a
heyn2 <hy702@gachon.ac.kr>
2025-04-23 19:02:47 +0900

    🐛 fix: prefill page에서 타입 오류 수정 (params 타입 정의)

--- src/app/write/prefill/[generatedPostId]/page.tsx
+++ src/app/write/prefill/[generatedPostId]/page.tsx
@@ -1,11 +1,14 @@
+// src/app/write/prefill/[generatedPostId]/page.tsx
 import PrefillClientWrapper from "./PrefillClientWrapper";
 
-type PrefillPageProps = {
+interface PageProps {
   params: {
     generatedPostId: string;
   };
-};
+}
 
-export default function Page({ params }: PrefillPageProps) {
-  return <PrefillClientWrapper generatedPostId={params.generatedPostId} />;
+export default function Page({ params }: PageProps) {
+  const { generatedPostId } = params;
+
+  return <PrefillClientWrapper generatedPostId={generatedPostId} />;
 }
