449f286992c3f8662a94bf32c2f3c63c3667fb2c
heyn2 <hy702@gachon.ac.kr>
2025-04-24 18:09:04 +0900

    layou 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
 // src/app/layout.tsx
-+ "use client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "@styles/globalStyles";
 import theme from "@styles/theme";
 import { gowunBatang } from "@styles/fonts";
-import Script from "next/script";
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
