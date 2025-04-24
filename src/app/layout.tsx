4443e2967c1d58e4081b4c198ac0ea2db4300063
heyn2 <hy702@gachon.ac.kr>
2025-04-24 18:05:54 +0900

    layou 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
 // src/app/layout.tsx
-
++ "use client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "@styles/globalStyles";
 import theme from "@styles/theme";
 import { gowunBatang } from "@styles/fonts";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body className={gowunBatang.className}>
-     <ThemeProvider theme={theme}>
-         <GlobalStyle />
-          {/* GTM 스크립트가 필요하면 여기에 or Client 컴포넌트로 분리 */}
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
           {children}
-       </ThemeProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
