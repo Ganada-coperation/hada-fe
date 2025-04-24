75d881d4894fbd19f6dd9479a918c398a2939f32
heyn2 <hy702@gachon.ac.kr>
2025-04-24 18:03:55 +0900

    layout, head 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-// src/app/layout.tsx  <-- 이제 서버 컴포넌트
+// src/app/layout.tsx
+
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "@styles/globalStyles";
 import theme from "@styles/theme";
@@ -8,10 +9,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body className={gowunBatang.className}>
-        {/* GTM 스크립트는 Client Side가 필요하니,
-            이 부분만 Client 컴포넌트로 분리하거나
-            next/script를 여기서 여전히 사용해도 됩니다 */}
-        {children}
+     <ThemeProvider theme={theme}>
+         <GlobalStyle />
+          {/* GTM 스크립트가 필요하면 여기에 or Client 컴포넌트로 분리 */}
+          {children}
+       </ThemeProvider>
       </body>
     </html>
   );
